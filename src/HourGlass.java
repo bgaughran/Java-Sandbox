9c7addf221110420ffd3567d3669e71548991e98
bgaughran <gaughran@gmail.com>
2019-06-14 01:54:57 +0100

    Java doodling

--- src/HourGlass.java
+++ src/HourGlass.java
@@ -1,19 +1,81 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class HourGlass {
 
-    // Complete the hourglassSum function below.
+    private static final Scanner scanner = new Scanner(System.in);
+
     static int hourglassSum(int[][] arr) {
-        return 0;
+
+        List<Integer> hourGlassSums = new ArrayList<>();
+
+        /*
+            Background....
+                Given a  2D Array, arr
+                We define an hourglass in A
+                There are 16 hourglasses in A (4*4), and an hourglass sum is the sum of an hourglass' values.
+
+            Algorithm...
+                Calculate the hourglass sum for every hourglass in , then return the maximum hourglass sum.
+                    Get the elements of the array, sum & store sum
+                    Return largest number
+         */
+        hourGlassSums.add(sumOneHourGlass(0, 0, arr));
+        hourGlassSums.add(sumOneHourGlass(0, 1, arr));
+        hourGlassSums.add(sumOneHourGlass(0, 2, arr));
+        hourGlassSums.add(sumOneHourGlass(0, 3, arr));
+
+        hourGlassSums.add(sumOneHourGlass(1, 0, arr));
+        hourGlassSums.add(sumOneHourGlass(1, 1, arr));
+        hourGlassSums.add(sumOneHourGlass(1, 2, arr));
+        hourGlassSums.add(sumOneHourGlass(1, 3, arr));
+
+        hourGlassSums.add(sumOneHourGlass(2, 0, arr));
+        hourGlassSums.add(sumOneHourGlass(2, 1, arr));
+        hourGlassSums.add(sumOneHourGlass(2, 2, arr));
+        hourGlassSums.add(sumOneHourGlass(2, 3, arr));
+
+        hourGlassSums.add(sumOneHourGlass(3, 0, arr));
+        hourGlassSums.add(sumOneHourGlass(3, 1, arr));
+        hourGlassSums.add(sumOneHourGlass(3, 2, arr));
+        hourGlassSums.add(sumOneHourGlass(3, 3, arr));
+
+        return maxOfHourGlassSums(hourGlassSums);
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
+    static int sumOneHourGlass(int rowStart, int colStart, int[][] arr) {
+
+        int sum = 0;
+        sum += arr[rowStart][colStart];
+        sum += arr[rowStart][colStart + 1];
+        sum += arr[rowStart][colStart + 2];
+        sum += arr[rowStart + 1][colStart + 1];
+        sum += arr[rowStart + 2][colStart];
+        sum += arr[rowStart + 2][colStart + 1];
+        sum += arr[rowStart + 2][colStart + 2];
+
+
+        return sum;
+    }
+
+    static int maxOfHourGlassSums(List hourGlassSums) {
+
+        int max = -2147483648;
+
+        for (Object hourGlassSum : hourGlassSums) {
+
+            int nextInt = (Integer) hourGlassSum;
+            if (nextInt > max) {
+                max = nextInt;
+            }
+        }
+
+        return max;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
--- test/HourGlassTest.java
+++ test/HourGlassTest.java
@@ -1,18 +1,49 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HourGlassTest {
 
     @Test
-    void testRepeatedString() {
+    void hourglassSum() {
         int[][] twoDArray = new int[][]{
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+                { 1, 1, 1, 0, 0, 0},
+                { 0, 1, 0, 0, 0, 0},
+                { 1, 1, 1, 0, 0, 0},
+                { 0, 0, 2, 4, 4, 0},
+                { 0, 0, 0, 2, 0, 0},
+                { 0, 0, 1, 2, 4, 0}
         };
-        assertEquals(0, HourGlass.hourglassSum(twoDArray));
+        assertEquals(19, HourGlass.hourglassSum(twoDArray));
     }
+
+    @Test
+    void sumOfHourGlassSums() {
+        List<Integer> hourGlassSums = new ArrayList<>();
+        hourGlassSums.add(1);
+        hourGlassSums.add(-1);
+        hourGlassSums.add(999);
+        hourGlassSums.add(-99);
+        assertEquals(999, HourGlass.maxOfHourGlassSums(hourGlassSums));
+    }
+
+    @Test
+    void sumOneHourGlass() {
+        int[][] twoDArray = new int[][]{
+                { 1, 1, 1, 0, 0, 0},
+                { 0, 1, 0, 0, 0, 0},
+                { 1, 1, 1, 0, 0, 0},
+                { 0, 0, 2, 4, 4, 0},
+                { 0, 0, 0, 2, 0, 0},
+                { 0, 0, 1, 2, 4, 0}
+        };
+
+        assertEquals(7, HourGlass.sumOneHourGlass(0, 0 , twoDArray));
+        assertEquals(19, HourGlass.sumOneHourGlass(3, 2 , twoDArray));
+    }
+
+
 }
